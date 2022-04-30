71b1807cb70a5092be359404087198ad18ee92b6
Roy <mangappu.pardamean.simanjuntak19@mhs.ubharajaya.ac.id>
2022-04-30 11:58:40 +0700

    Penmabahan sidebar menu JS

--- resources/views/items/sidebar_admin.blade.php
+++ resources/views/items/sidebar_admin.blade.php
@@ -97,7 +97,7 @@
 				</li><li>
 					<a href='?module=JS Arrays'><i class='fa fa-angle-double-right'></i>JS Arrays[ GALIH ADI PRATAMA]</a>
 				</li><li>
-					<a href='?module=JS Array Methods'><i class='fa fa-angle-double-right'>	   </i>JS Array Methods[ 		]</a>
+					<a href="{{ route('jsmangapu') }}"><i class='fa fa-angle-double-right'>	   </i>JS Mangapu Pardamean[ 	MANGAPU PARDAMEAN		]</a>
 				</li><li>
 					<a href='?module=JS Array Sort'><i class='fa fa-angle-double-right'>		  </i>JS Array Sort[	]</a>
 				</li><li>
